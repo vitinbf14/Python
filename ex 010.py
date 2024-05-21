ea16a5bd60628066b155725f9a34538619c15892
Vitor <136298994+vitinbf14@users.noreply.github.com>
2024-05-21 14:23:10 -0300

    Letra "A"
    Este código solicita uma frase ao usuário, converte-a para maiúsculas e exibe quantas vezes a letra "A" aparece, a posição da primeira ocorrência de "A" e a posição da última ocorrência de "A".

---
+++ ex 010.py
@@ -0,0 +1,6 @@
+frase = str(input('Digite uma frase:')).upper()
+print('A letra A aparece {} vezes na frase'.format(frase.count('A')))
+print('A primeira lera A apareceu na posição {}'.format(frase.find('A')+1))
+print('A última letra A apareceu na posição {}'.format(frase.rfind('A')+1))
+
+
